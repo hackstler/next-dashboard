e5b2640facdc028c9f854f0e64e04e1f382d3e45
hackstler <sergio@hackstler.com>
2024-03-23 21:03:00 +0100

    complete next tutorial

---
+++ app/ui/fonts.ts
@@ -0,0 +1,8 @@
+import { Montserrat, Lusitana } from 'next/font/google';
+
+export const montserrat = Montserrat({ subsets: ['latin'] });
+export const lusitana = Lusitana({
+  style: 'normal',
+  weight: '400',
+  subsets: ['latin'],
+});
